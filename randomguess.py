8e3bbb2572f8a38d03aaf6008fbcb33b4b5682c0
Eswarnath-jeyaram <eswarnathj@gmail.com>
2025-08-02 20:40:07 +0530

    random guessing

---
+++ randomguess.py
@@ -0,0 +1,18 @@
+print ("WELCOME TO THE GUESSING GAME!!!")
+import random 
+num = random.randint(1,100)
+trial = 0
+max_trial = 6
+while trial < max_trial :
+     guessnum = int(input("Guess a number between 1 - 100 :"))
+     if guessnum == num :
+         print(f"HURRAH YOU WON THE GAME YOUR GUESS {guessnum} IS CORRECT !!!")
+         break
+     elif guessnum < num :
+         print("WRONG YOUR GUESS IS TOO LOW TRY AGAIN")
+         trial = trial + 1
+     else :
+        print ("WRONG YOUR GUESS IS TOO HIGH TRY AGAIN")
+        trial = trial + 1
+if trial == max_trial and guessnum != num :
+     print(f"YOU LOSE THE CORRECT NUMBER IS {num}")
